4a272048226e38ca86293cc03a16fb1f76386a48
NtrLk <naritoplesio@gmail.com>
2022-05-17 18:39:01 +0200

    asd

--- e-shop-manager/src/main/java/it/unipd/mtss/Articolo.java
+++ e-shop-manager/src/main/java/it/unipd/mtss/Articolo.java
@@ -2,25 +2,12 @@ package it.unipd.mtss;
 
 public class Articolo implements EItem{
 	String nome;
-	int prezzo;
+	Integer prezzo;
 	enum itemType {Processore,SchedeMadri, Mouse, Tastiere};
 	
-	public Articolo(String nome, int prezzo) {
-		if(nome != null) {
-			if(nome.length() > 0 && prezzo > 0)
-			{
-				this.nome = nome;
-				this.prezzo = prezzo;
-			}
-			else
-			{
-				System.out.println("I prezzi non possono essere minori o uguali a zero | un articolo deve avere un nome");
-			}
-		}
-		else
-		{
-			System.out.println("Un articolo deve avere un nome");
-		}
+	public Articolo(String nome, Integer prezzo) {
+		this.nome = nome;
+		this.prezzo = prezzo;
 	}
 	public int getPrezzo() {
 		return this.prezzo;
@@ -29,8 +16,4 @@ public class Articolo implements EItem{
 	public String getNome() {
 		return this.nome;
 	}
-	
-	/* Test getter / test costruttore  */
-	
-	
 }
--- e-shop-manager/src/test/java/it/unipd/mtss/ArticoloTest.java
+++ e-shop-manager/src/test/java/it/unipd/mtss/ArticoloTest.java
@@ -1,16 +1,60 @@
 package it.unipd.mtss;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ArticoloTest {
 
-	
+	Articolo articolo;
+	Articolo articoloDue;
+    @Before
+    public void setUp()
+    {
+    	articolo = new Articolo(" ",0);
+    	articoloDue = new Articolo("A",100);
+    }
 	
 	@Test
-	public void test() {
+	public void getPrezzo()
+	{
+		assertEquals(100,articolo.getPrezzo());
+	}
+	
+	@Test
+	public void getNome()
+	{
+		
+		
+		/* */
+		
+		assertEquals("A",articolo.getNome());
+	}
+	
+	@Test
+	public void notNullNome()
+	{
+		
+			
+		assertNotNull(articolo.getNome());
+	}
+	
+	@Test
+	public void notNullPrezzo()
+	{
+		
+			
+		assertNotNull(articolo.getPrezzo());
+	}
+	
+	@Test
+	public void prezzoMaggioreDiZero()
+	{
+		Articolo articolo = new Articolo(" ",-5);
+			
 		
 	}
-
+	
 }
