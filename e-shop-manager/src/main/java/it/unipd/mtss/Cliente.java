8a2f6e4b206a901ae66d4c036e9a0cef38c45b92
RaulSeganfreddo <rseganfreddo@gmail.com>
2022-05-20 10:11:22 +0200

    Aggiunte eccezioni a Cliente.java

--- e-shop-manager/src/main/java/it/unipd/mtss/Cliente.java
+++ e-shop-manager/src/main/java/it/unipd/mtss/Cliente.java
@@ -7,6 +7,18 @@ public class Cliente implements User{
 	String luogoNascita;
 	
 	public Cliente(int age, String nome, String cognome, String luogoNascita) {
+		if (nome == null) {
+			throw new IllegalArgumentException("nome non può essere null");
+		}
+		if (cognome == null) {
+			throw new IllegalArgumentException("cognome non può essere null");
+		}
+		if (luogoNascita == null) {
+			throw new IllegalArgumentException("luogo di nascita non può essere null");
+		}
+		if (age < 10) {
+			throw new IllegalArgumentException("age non può essere negativo");
+		}
 		this.nome = nome;
 		this.cognome = cognome;
 		this.luogoNascita = luogoNascita;
